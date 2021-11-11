30887b7a3107444df3bc2eca640ab62f43cec611
nate2411 <nathandj28@gmail.com>
2021-11-11 12:19:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log('Hello world');
+
+
+// Variable//
+// let name = 'Nathan';
+// console.log(name);
+
+// variable cannot be a reserved keyword
+//should be meaningful name
+// Cannot start with a number
+// Cannot contain a space or a hyphen
+
+// let firstName = 'Nathan';
+// let lastName = 'De Jager';
+
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+
